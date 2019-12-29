bc5e88e18210d43d9820a54141859b265b2d8bac
Moritz <22394314+movd@users.noreply.github.com>
2019-12-29 21:18:22 +0100

    Finish Ex. 6.16

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
-const App = () => {
+import anecdotesService from "./services/anecdotes";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
+import anecdotes from "./services/anecdotes";
+
+const App = props => {
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then(anecdotes => props.initializeAnecdotes(anecdotes));
+  });
+
   return (
     <div>
       <h2>Programming anecdotes</h2>
@@ -13,4 +24,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default connect(null, { initializeAnecdotes })(App);
